ca2fd59a4c19ed3ca630271813287028476c83ff
Ibtisam Ahmad <ahmadebtisam3@gmail.com>
2024-12-25 19:19:48 +0500

    Update setup script

--- setup_and_update.bat
+++ setup_and_update.bat
@@ -1,5 +1,6 @@
 if not exist .env ( python -m venv .env )
 
+git pull origin master
 .\.env\Scripts\pip.exe install -r .\lms\requirements.txt
 .\.env\Scripts\python.exe .\lms\manage.py migrate
 npm install
